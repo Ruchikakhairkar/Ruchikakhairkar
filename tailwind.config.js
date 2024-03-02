efa2e041f6e5cc995af3a176d82e7d2a9749e701
Ruchika Khairkar <khairkarruchika2@gmail.com>
2024-03-02 10:11:12 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        navbarColor: "#ebfaf7",
+        darkblue: "#151269",
+        blackpurple: "#6b21a8",
+        darkpurple: "#7e22ce",
+        purple: "#9333ea",
+        lightpurple: "#a855f7",
+        faintpurple: "#c084fc",
+      },
+    },
+  },
+  plugins: [],
+};
